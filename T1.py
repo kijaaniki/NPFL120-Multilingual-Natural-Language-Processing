502f29c72b45a2f7686b98f71de15206b9dac17a
sundroid <62110053+kijaaniki@users.noreply.github.com>
2020-07-06 21:21:41 +0200

    WALS assignment

---
+++ T1.py
@@ -0,0 +1,71 @@
+#Task 1: WALS code = 0
+#Task 2: Genus = 6
+
+import numpy as np
+from collections import Counter
+
+def jaccard(list1, list2):
+    intersection = len(list(set(list1).intersection(list2)))
+    union = (len(list1) + len(list2)) - intersection
+    return float(intersection) / union
+
+def getList(dict): 
+    return dict.keys() 
+
+Lang_Data = []
+
+data = open("language.tsv").read().split('\n')
+
+for i in range(1,len(data)):
+    el = data[i].split("\t")
+    El = []
+    if not len(el)==1:
+        Lang_Data.append(el)
+
+l_data = np.asarray(Lang_Data)[:,4:]
+wals = (np.asarray(Lang_Data)[:,0]).tolist()
+
+Meta = []
+
+for col in range(l_data.shape[1]):
+    u = (np.unique(l_data[:,col])).tolist()
+    D = {}
+    for item in range(len(u)):
+        i = u[item]
+        if i == '':
+            D[i]='NaN'
+        else:
+            D[i] = item
+    Meta.append(D)
+
+Data_Mod = []
+
+for row in range(l_data.shape[0]):
+    line = l_data[row]
+    C = []
+    for col in range(len(line)):
+        c = l_data[row,col]
+        m = Meta[col]
+        C.append(m[c])
+    Data_Mod.append(C)
+
+Data_M = np.asarray(Data_Mod)
+       
+t = input("Enter WALS code: ")
+ind = ' '
+
+for name in wals:
+    if name==t:
+        ind = wals.index(name)
+
+
+Langs = {}
+for lang in range(len(l_data)):
+    if lang!=ind:
+        j= jaccard(Data_M[ind].tolist(),Data_M[lang].tolist())
+        Langs[wals[lang]] = j
+
+c = Counter(Langs)
+mc = c.most_common(5)
+print(mc)
+
